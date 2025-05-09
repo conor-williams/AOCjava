f45bbbb79840b4b26f3e6473b30ea4c704ae376f
C.P Williams <conor.williams@gmail.com>
2025-05-09 20:16:27 +0100

    Add files via upload

--- ALL.2016/year2016_day2_2.java
+++ ALL.2016/year2016_day2_2.java
@@ -15,7 +15,7 @@ import java.util.Set;
 import java.util.Iterator;
 import java.util.Objects;
 
-class year2016_day2 {
+class year2016_day2_2 {
 	public static char [][] keypad = {{'0', '0', '0', '0', '0', '0', '0'},
 		{'0', '0', '0', '1', '0', '0', '0'}, {'0', '0', '2', '3', '4', '0', '0'},
 		{'0', '5', '6', '7', '8', '9', '0'}, {'0', '0', 'A', 'B', 'C', '0', '0'},
