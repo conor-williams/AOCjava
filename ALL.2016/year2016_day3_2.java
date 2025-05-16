9c8df2c76c281d184ed7533541fc6bf314d26ab6
C.P Williams <conor.williams@gmail.com>
2025-05-16 18:06:04 +0100

    clear

---
+++ ALL.2016/year2016_day3_2.java
@@ -0,0 +1,120 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import static java.lang.System.out;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Collections;
+import java.lang.Character;
+import java.util.HashMap;
+import java.util.Map;
+
+
+class year2016_day3_2 {
+	public static int [][] keypad = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	public static void main(String [] args) {
+		out.println("		2016 Day3.2");
+		Vector<String> blah = new Vector<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				blah.add(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+
+		Pattern p = Pattern.compile("(\\d+)[\t\\s ]+(\\d+)[\t\\s ]+(\\d+)");
+		//	Matcher m = p.matcher("17-MAR-11 15.52.25.000000000");
+		//	m.find();
+		//	month= m.group(2);
+
+		// Vector <Tuple <Integer, String>> ve1 = new Vector<>();
+		//BigInteger tot =  BigInteger.valueOf((long)0);
+		int validTriangle = 0;
+		for (int i = 0; i < blah.size(); i+=3) {
+			Matcher m1 = p.matcher(blah.get(i));
+			Matcher m2 = p.matcher(blah.get(i+1));
+			Matcher m3 = p.matcher(blah.get(i+2));
+
+			m1.find(); m2.find(); m3.find();
+
+			Vector <Integer> var_ints1 = new Vector<>();
+			Vector <Integer> var_ints2 = new Vector<>();
+			Vector <Integer> var_ints3 = new Vector<>();
+
+			var_ints1.add(Integer.valueOf(m1.group(1)));
+			var_ints1.add(Integer.valueOf(m2.group(1)));
+			var_ints1.add(Integer.valueOf(m3.group(1)));
+			//out.print(var_ints1.get(0)); out.print(" "); out.print(var_ints1.get(1)); out.print(" ");
+			//out.println(var_ints1.get(2));
+
+			var_ints2.add(Integer.valueOf(m1.group(2)));
+			var_ints2.add(Integer.valueOf(m2.group(2)));
+			var_ints2.add(Integer.valueOf(m3.group(2)));
+
+			var_ints3.add(Integer.valueOf(m1.group(3)));
+			var_ints3.add(Integer.valueOf(m2.group(3)));
+			var_ints3.add(Integer.valueOf(m3.group(3)));
+
+			if ((var_ints1.get(0) + var_ints1.get(1) > var_ints1.get(2)) 
+					&& (var_ints1.get(0) + var_ints1.get(2) > var_ints1.get(1))
+					&& (var_ints1.get(1) + var_ints1.get(2) > var_ints1.get(0))) {
+				validTriangle++;
+					}
+			if ((var_ints2.get(0) + var_ints2.get(1) > var_ints2.get(2)) 
+					&& (var_ints2.get(0) + var_ints2.get(2) > var_ints2.get(1))
+					&& (var_ints2.get(1) + var_ints2.get(2) > var_ints2.get(0))) {
+				validTriangle++;
+					}
+			if ((var_ints3.get(0) + var_ints3.get(1) > var_ints3.get(2)) 
+					&& (var_ints3.get(0) + var_ints3.get(2) > var_ints3.get(1))
+					&& (var_ints3.get(1) + var_ints3.get(2) > var_ints3.get(0))) {
+				validTriangle++;
+					}
+
+		}
+		out.print("**j_ans: ");
+		out.print(validTriangle);
+		out.println("");
+	}
+}
+
+public class Tuple<X,Y > {
+	public final X first;
+	public final Y second;
+
+	public Tuple(X first, Y second) {
+		this.first = first;
+		this.second = second;
+	}
+	@Override
+	public boolean equals(Object o) {
+		Tuple tu2 = (Tuple) o;
+		if (this == o) return true;
+		if (!(o instanceof Tuple)) return false;
+		int fir1 = (int)first;
+		int fir2 = (int)tu2.first;
+		int sec1 = (int)second;
+		int sec2 = (int)tu2.second;
+		if (fir1 != fir2) {return false;}
+		if (sec1 != sec2) {return false;}
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second);
+	}
+
+}
+
