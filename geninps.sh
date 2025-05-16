43c0f915dc06e4e14402bb8314ec98a58813ae5d
C.P Williams <conor.williams@gmail.com>
2025-05-16 20:09:10 +0100

    for creating specific to the user inputs

---
+++ geninps.sh
@@ -0,0 +1,6 @@
+rm inp.tar 
+rm inp.tar.gz
+find 20* -name \*i1.txt -o -name i2.txt -o -name i3.txt -o -name \*ex\*.txt | xargs tar -rvf inp.tar
+gzip inp.tar
+cp inp.tar.gz input-2015-2014.tar.gz
+echo "move to input-2015-2014.tar.gz"
