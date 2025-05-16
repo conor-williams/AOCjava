5554ef7c941ca52a3ec90eb8222c940138b74637
C.P Williams <conor.williams@gmail.com>
2025-05-16 20:07:20 +0100

    readying for java

--- specialCopy.sh
+++ specialCopy.sh
@@ -4,11 +4,15 @@ day=1
 
 while [ $year != 2025 ] ; do
 	while [ $day != 26 ] ; do
-		cp ALL.$year/year${year}_day${day}.java $year/day$day/
-		cp ALL.$year/year${year}_day${day}* $year/day$day/
-		if [ $day != 25 ] ; then
+		if [ -f ALL.$year/year${year}_day${day}.java ]; then
+			cp ALL.$year/year${year}_day${day}.java $year/day$day/
+		fi
+		if [ -f ALL.$year/year${year}_day${day}_2.java ]; then
 			cp ALL.$year/year${year}_day${day}_2.java $year/day$day/
 		fi
+#		if [ $day != 25 ] ; then
+#			cp ALL.$year/year${year}_day${day}_2.java $year/day$day/
+#		fi
 		day=$(($day+1))
         done
 	day=1
