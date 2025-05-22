abd2c18512915ab86c756d7b968808a812053dac
C.P Williams <conor.williams@gmail.com>
2025-05-22 16:35:58 +0100

    q6

---
+++ ALL.2021/year2021_day6.java
@@ -0,0 +1,125 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import static java.lang.System.out;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Collections;
+import java.lang.Character;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+import java.io.*;
+import java.util.ArrayDeque;
+import java.util.Deque;
+// /java -Xmx2g year2019_day3.java *i1.txt
+
+
+//System.setOut(originalOut);
+//PrintStream originalOut = System.out;
+//System.setOut(new PrintStream(new java.io.OutputStream() { public void write(int b) { } }));
+//Scanner scanner = new Scanner(System.in); scanner.nextLine();
+// int max = var_ints.stream().max(Integer::compare).orElseThrow();
+// int position = var_ints.indexOf(max);
+
+class year2021_day6 {
+	public static int [][] keypad = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	public static void main(String [] args) {
+		out.println("		2021 Day6.1");
+		Vector<String> blah = new Vector<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				blah.add(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+
+		//	String firstpart = Pattern.quote("mul(");
+		//Pattern p = Pattern.compile("(L|R)(\\d+)");
+		//	Matcher m = p.matcher("17-MAR-11 15.52.25.000000000");
+		//	m.find();
+		//
+		//	month= m.group(2);
+
+		// Vector <Tuple <Integer, String>> ve1 = new Vector<>();
+		//BigInteger tot =  BigInteger.valueOf((long)0);
+		Vector <Integer> lfish = new Vector<>();
+		int [] count = new int [10];
+		for (int i = 0; i < blah.size(); i++) {
+			   Scanner scanner = new Scanner(blah.get(i));
+			   scanner.useDelimiter(",");
+			   while (scanner.hasNext()) {
+			   	String ne = scanner.next();
+			   	count[Integer.valueOf(ne)]++;
+			   }
+		}
+
+		for (int ii = 0; ii <= 8; ii++) {
+			lfish.addLast(count[ii]);
+		}
+		
+		int loop = 0;
+		while (loop < 80) {
+			Integer head1 = lfish.removeFirst();
+			lfish.addLast(head1);
+			Integer tail1 = lfish.removeLast();
+			Integer tail2 = lfish.removeLast();
+			Integer six = lfish.removeLast();
+			lfish.addLast(head1+six);
+			lfish.addLast(tail2);
+			lfish.addLast(tail1);
+			loop++;
+		}
+
+
+		int cou = 0;
+		for (int element: lfish) {
+			System.out.print(element + " ");
+			cou += element;
+		}
+		out.print("**j_ans: ");
+		out.print(cou);
+		out.println("");
+	}
+}
+
+class Tuple<X,Y > {
+	public final X first;
+	public final Y second;
+
+	public Tuple(X first, Y second) {
+		this.first = first;
+		this.second = second;
+	}
+	@Override
+	public boolean equals(Object o) {
+		Tuple tu2 = (Tuple) o;
+		if (this == o) return true;
+		if (!(o instanceof Tuple)) return false;
+		int fir1 = (int)first;
+		int fir2 = (int)tu2.first;
+		int sec1 = (int)second;
+		int sec2 = (int)tu2.second;
+		if (fir1 != fir2) {return false;}
+		if (sec1 != sec2) {return false;}
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second);
+	}
+
+}
+
