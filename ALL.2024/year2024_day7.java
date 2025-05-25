194b775d0c0f66882dc7b1d1388b9d87c22cec5d
C.P Williams <conor.williams@gmail.com>
2025-05-25 21:05:07 +0100

    q7

---
+++ ALL.2024/year2024_day7.java
@@ -0,0 +1,150 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import static java.lang.System.out;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Collections;
+import java.lang.Character;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+// /java -Xmx2g year2019_day3.java *i1.txt
+
+
+//		PrintStream originalOut = System.out;
+//		System.setOut(new PrintStream(new java.io.OutputStream() { public void write(int b) { } }));
+//		System.setOut(originalOut);
+//Scanner scanner = new Scanner(System.in); scanner.nextLine();
+// int max = var_ints.stream().max(Integer::compare).orElseThrow();
+// int position = var_ints.indexOf(max);
+
+//                        for (Map.Entry<Character, Vector<Character>> entry : mp.entrySet()) {
+// System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+//}
+
+class year2024_day7 {
+	public static int [][] keypad = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	public static void main(String [] args) {
+		out.println("		2024 Day7.1");
+		Vector<String> blah = new Vector<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				blah.add(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+
+		//	String firstpart = Pattern.quote("mul(");
+		Pattern p = Pattern.compile("([\\d]+): ([\\s\\d]+)");
+		//	Matcher m = p.matcher("17-MAR-11 15.52.25.000000000");
+		//	m.find();
+		//
+		//	month= m.group(2);
+
+		// Vector <Tuple <Integer, String>> ve1 = new Vector<>();
+		//BigInteger tot =  BigInteger.valueOf((long)0);
+		BigInteger one =  new BigInteger("1");
+		BigInteger zero =  new BigInteger("0");
+		BigInteger tot = zero ;
+		for (int i = 0; i < blah.size(); i++) {
+			Matcher m = p.matcher(blah.get(i));
+			///out.println(blah.get(i));
+			m.find();
+			
+			BigInteger left = new BigInteger(m.group(1));
+			String right = m.group(2);
+
+			Scanner scanner = new Scanner(right);
+			scanner.useDelimiter("[\t\\s]+");
+			Vector <BigInteger> var_ints = new Vector<>();
+			while (scanner.hasNext()) {
+				String ne = scanner.next();
+				var_ints.add(new BigInteger(ne));
+			}
+
+			double fr = 2;
+			double en = var_ints.size();
+			int end = (int)Math.pow(fr, en);
+
+			for (int kk = 0; kk < end; kk++) { // 15 in decimal is 1111 in binary
+				//out.print("kk: "); out.println(kk);
+				//out.print("Integer.toString(2)): "); out.println(Integer.toString(kk, 2));
+				String binary = padLeftZeros(Integer.toString(kk, 2), 20);
+				//String binary = String.format("%020d", Integer.toString(kk, 2));
+				//out.println(binary);
+				String rev = new StringBuilder(binary).reverse().toString();
+				BigInteger val = var_ints.get(0);
+				for (int jj = 1; jj < var_ints.size(); jj++) {
+					if (rev.charAt(jj-1) == '0') {
+						val = val.add(var_ints.get(jj));
+					} else {
+						val = val.multiply(var_ints.get(jj));
+					}
+				}
+				if (val.compareTo(left) == 0) {
+					tot = tot.add(left);
+					break;
+				}
+			}
+
+		}
+		out.print("**j_ans: ");
+		out.print(tot);
+		out.println("");
+	}
+	public static String padLeftZeros(String inputString, int length) {
+		if (inputString.length() >= length) {
+			return inputString;
+		}
+		StringBuilder sb = new StringBuilder();
+		while (sb.length() < length - inputString.length()) {
+			sb.append('0');
+		}
+		sb.append(inputString);
+		return sb.toString();
+	}
+}
+
+class Tuple<X,Y > {
+	public final X first;
+	public final Y second;
+
+	public Tuple(X first, Y second) {
+		this.first = first;
+		this.second = second;
+	}
+	@Override
+	public boolean equals(Object o) {
+		Tuple tu2 = (Tuple) o;
+		if (this == o) return true;
+		if (!(o instanceof Tuple)) return false;
+		int fir1 = (int)first;
+		int fir2 = (int)tu2.first;
+		int sec1 = (int)second;
+		int sec2 = (int)tu2.second;
+		if (fir1 != fir2) {return false;}
+		if (sec1 != sec2) {return false;}
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second);
+	}
+
+}
+
