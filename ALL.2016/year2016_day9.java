9dd329674ea055a01e36fde3852af5812b871eaf
C.P Williams <conor.williams@gmail.com>
2025-05-29 20:48:41 +0100

    q9

---
+++ ALL.2016/year2016_day9.java
@@ -0,0 +1,157 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import static java.lang.System.out;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Collections;
+import java.lang.Character;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+// /java -Xmx2g year2019_day3.java *i1.txt
+
+
+//Scanner scanner = new Scanner(System.in); scanner.nextLine();
+// int max = var_ints.stream().max(Integer::compare).orElseThrow();
+// int position = var_ints.indexOf(max);
+
+//                        for (Map.Entry<Character, Vector<Character>> entry : mp.entrySet()) {
+                                // System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+//}
+
+@SuppressWarnings("unchecked")
+class year2016_day9 {
+	public static int [][] keypad = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	public static void main(String [] args) {
+		out.println("		2016 Day9.1");
+		Vector<String> blah = new Vector<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				blah.add(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+//		PrintStream originalOut = System.out;
+//		System.setOut(new PrintStream(new java.io.OutputStream() { public void write(int b) { } }));
+
+		//	String firstpart = Pattern.quote("mul(");
+		Pattern p = Pattern.compile("\\((\\d+)x(\\d+)\\)");
+		//	Matcher m = p.matcher("17-MAR-11 15.52.25.000000000");
+		//	m.find();
+		//
+		//	month= m.group(2);
+
+		// Vector <Tuple <Integer, String>> ve1 = new Vector<>();
+		//BigInteger tot =  BigInteger.valueOf((long)0);
+		//BigInteger one =  new BigInteger("1");
+		//BigInteger zero =  new BigInteger("0");
+		int wlen = blah.get(0).length();
+		long [] weights = new long[wlen];
+		for (int ii = 0; ii < wlen; ii++) {
+			weights[ii] = 1;
+		}
+		long len = 0;
+		Vector <Tuple <Integer, Integer>> ve = new Vector<>();
+		for (int i = 0; i < blah.size(); i++) {
+			String ne = blah.get(i);
+
+			for (int ii = 0; ii < ne.length(); ii++) {
+				if (ne.charAt(ii) == '(') {
+					int end = ne.indexOf(')', ii)+1;
+					String result = ne.substring(ii, end);
+					//out.println(result);
+					//Scanner scanner = new Scanner(System.in); scanner.nextLine();
+					ii += result.length()-1;
+					Matcher m = p.matcher(result);
+					m.find();
+					//out.println(m.group(1));
+					//out.println(m.group(2));
+					
+					int were = Integer.valueOf(m.group(1));
+					int mul = Integer.valueOf(m.group(2));
+					for (int zz = end; zz < end+were; zz++) {
+						//weights[zz] *= mul;
+						len += mul;
+						ii++;
+
+					}
+					//ii--;
+					/*
+					while (ne.charAt(ii+1) == '(') {
+						out.println("here1");
+						len += weights[ii+1];
+						ii = ii+1;
+						while (ne.charAt(ii) != ')') {
+							out.println("here1");
+							len += weights[ii];
+							ii++;
+						}
+					}
+					assert(ne.charAt(ii+1) != '(');
+					*/
+				} else if (ne.charAt(ii) == ')') {
+					//len += weights[ii];
+					out.println("ERR end brace...");
+					Runtime.getRuntime().halt(0);
+				} else {
+					//out.println("here1");
+					//len += weights[ii];
+					len++;
+				}
+			}
+		}
+		/*
+		for (int kk = 0; kk < wlen; kk++) {
+			out.print(weights[kk]); out.print("|");
+		}
+		out.println();
+		*/
+		out.print("**j_ans: ");
+		out.print(len);
+		out.println("");
+	}
+}
+
+class Tuple<X,Y > {
+	public final X first;
+	public final Y second;
+
+	public Tuple(X first, Y second) {
+		this.first = first;
+		this.second = second;
+	}
+	@Override
+	public boolean equals(Object o) {
+		Tuple tu2 = (Tuple) o;
+		if (this == o) return true;
+		if (!(o instanceof Tuple)) return false;
+		int fir1 = (int)first;
+		int fir2 = (int)tu2.first;
+		int sec1 = (int)second;
+		int sec2 = (int)tu2.second;
+		if (fir1 != fir2) {return false;}
+		if (sec1 != sec2) {return false;}
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second);
+	}
+
+}
+
