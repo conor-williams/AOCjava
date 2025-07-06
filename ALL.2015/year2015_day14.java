cc24a62583b5f89dcf72552d0ece6689db9bece0
C.P Williams <conor.williams@gmail.com>
2025-07-06 19:07:08 +0100

    q14

---
+++ ALL.2015/year2015_day14.java
@@ -0,0 +1,182 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import static java.lang.System.out;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Collections;
+import java.lang.Character;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+// /java -Xmx2g year2019_day3.java *i1.txt
+
+
+//                        grid = Arrays.stream(gridTmp).map(char[]::clone).toArray(char[][]::new);
+// pe.sort(Comparator.comparingInt((TreTuple t) -> (int)t.third).thenComparingInt((TreTuple t) -> (int)t.second).thenComparingInt((TreTuple t) -> (int)t.first));///tuples.sort(Comparator.comparingInt(tuple -> tuple[0]));
+//Scanner scanner = new Scanner(System.in); scanner.nextLine();
+// int max = var_ints.stream().max(Integer::compare).orElseThrow();
+// int position = var_ints.indexOf(max);
+
+//                        for (Map.Entry<Character, Vector<Character>> entry : mp.entrySet()) {
+                                // System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+//}
+
+@SuppressWarnings("unchecked")
+class year2015_day14 {
+//	        public static int maxPath = 0;
+ //       public static int sx = 0;
+  //      public static int sy = 0;
+   //     public static char grid [][] = new char[sy][sx];
+    //    public static int already [][] = new int[sy][sx];
+
+	public static void main(String [] args) {
+		out.println("		2014 Day14.1");
+		out.flush();
+		Vector<String> blah = new Vector<>();
+		//int leny = 0;
+                //int lenx = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+                                //if (lenx == 0) {lenx = line.length();}
+				blah.add(line);
+				//leny++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+//		PrintStream originalOut = System.out;
+//		System.setOut(new PrintStream(new java.io.OutputStream() { public void write(int b) { } }));
+                /*sx = lenx;
+                sy = leny;
+                grid = new char[sy][sx];
+                already = new int[sy][sx];
+                for (int i = 0; i < blah.size();i++) {
+                        grid[i] = blah.get(i).toCharArray();
+                }
+		*/
+
+
+		//	String firstpart = Pattern.quote("mul(");
+		Pattern p = Pattern.compile("([A-Za-z]+) can fly (\\d+) km/s for (\\d+) seconds, but then must rest for (\\d+) seconds.");
+		//	Matcher m = p.matcher("17-MAR-11 15.52.25.000000000");
+		//	m.find();
+		//
+		//	month= m.group(2);
+
+		// Vector <Tuple <Integer, String>> ve1 = new Vector<>();
+		//BigInteger tot =  BigInteger.valueOf((long)0);
+		//BigInteger one =  new BigInteger("1");
+		//BigInteger zero =  new BigInteger("0");
+
+		Map <String, TreTuple <Integer, Integer, Integer>> mp = new HashMap<>();
+		for (int i = 0; i < blah.size(); i++) {
+			Matcher m = p.matcher(blah.get(i));
+			m.find();
+
+			String name = m.group(1);
+			int speed = Integer.valueOf(m.group(2));
+			int flytime = Integer.valueOf(m.group(3));
+			int rest = Integer.valueOf(m.group(4));
+
+			mp.put(name, new TreTuple(speed, flytime, rest));
+
+		}
+		int sec = 2503;
+		int max = 0;
+		for (var entry : mp.entrySet()) {
+			var tu1 = entry.getValue();
+			int speed = tu1.first;
+			int flytime = tu1.second;
+			int rest = tu1.third;
+
+			int dist1 = speed * flytime * (sec / (flytime+rest));
+			int left = sec % (flytime+rest);
+
+			if (left <= flytime) {
+				dist1 += (left * speed);
+			} else {
+				dist1 += (flytime * speed);
+			}
+			//out.println(dist1);
+			if (dist1 > max) {max = dist1;}
+		}
+//		System.setOut(originalOut);
+		out.print("**j_ans: ");
+		out.print(max);
+		out.println("");
+	}
+}
+
+class Tuple<X,Y > {
+	public final X first;
+	public final Y second;
+
+	public Tuple(X first, Y second) {
+		this.first = first;
+		this.second = second;
+	}
+	@Override
+	public boolean equals(Object o) {
+		Tuple tu2 = (Tuple) o;
+		if (this == o) return true;
+		if (!(o instanceof Tuple)) return false;
+		if (!this.first.equals(tu2.first)) {return false;}
+                if (!this.second.equals(tu2.second)) {return false;}
+
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second);
+	}
+
+}
+
+@SuppressWarnings("unchecked")
+class TreTuple<X,Y, Z> {
+        public X first;
+        public Y second;
+        public Z third;
+
+        public TreTuple(Object o) {
+                TreTuple tu2 = (TreTuple) o;
+                this.first = (X)tu2.first;
+                this.second = (Y)tu2.second;
+                this.third = (Z)tu2.third;
+        }
+        public TreTuple(X first, Y second, Z third) {
+                this.first = first;
+                this.second = second;
+                this.third = third;
+        }
+        @Override
+        public boolean equals(Object o) {
+                TreTuple tu2 = (TreTuple) o;
+                if (this == o) return true;
+                if (!(o instanceof TreTuple)) return false;
+                if (!this.first.equals(tu2.first)) {return false;}
+                if (!this.second.equals(tu2.second)) {return false;}
+                if (!this.third.equals(tu2.third)) {return false;}
+                return true;
+        }
+        @Override
+        public int hashCode() {
+                return Objects.hash(first, second, third);
+        }
+
+}
+
---
+++ ALL.2015/year2015_day14_2.java
@@ -0,0 +1,208 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import static java.lang.System.out;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Collections;
+import java.lang.Character;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+// /java -Xmx2g year2019_day3.java *i1.txt
+
+
+//                        grid = Arrays.stream(gridTmp).map(char[]::clone).toArray(char[][]::new);
+// pe.sort(Comparator.comparingInt((TreTuple t) -> (int)t.third).thenComparingInt((TreTuple t) -> (int)t.second).thenComparingInt((TreTuple t) -> (int)t.first));///tuples.sort(Comparator.comparingInt(tuple -> tuple[0]));
+//Scanner scanner = new Scanner(System.in); scanner.nextLine();
+// int max = var_ints.stream().max(Integer::compare).orElseThrow();
+// int position = var_ints.indexOf(max);
+
+//                        for (Map.Entry<Character, Vector<Character>> entry : mp.entrySet()) {
+                                // System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+//}
+
+@SuppressWarnings("unchecked")
+class year2015_day14_2 {
+//	        public static int maxPath = 0;
+ //       public static int sx = 0;
+  //      public static int sy = 0;
+   //     public static char grid [][] = new char[sy][sx];
+    //    public static int already [][] = new int[sy][sx];
+
+	public static void main(String [] args) {
+		out.println("		2014 Day14.2");
+		out.flush();
+		Vector<String> blah = new Vector<>();
+		//int leny = 0;
+                //int lenx = 0;
+		try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+                                //if (lenx == 0) {lenx = line.length();}
+				blah.add(line);
+				//leny++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+//		PrintStream originalOut = System.out;
+//		System.setOut(new PrintStream(new java.io.OutputStream() { public void write(int b) { } }));
+                /*sx = lenx;
+                sy = leny;
+                grid = new char[sy][sx];
+                already = new int[sy][sx];
+                for (int i = 0; i < blah.size();i++) {
+                        grid[i] = blah.get(i).toCharArray();
+                }
+		*/
+
+
+		//	String firstpart = Pattern.quote("mul(");
+		Pattern p = Pattern.compile("([A-Za-z]+) can fly (\\d+) km/s for (\\d+) seconds, but then must rest for (\\d+) seconds.");
+		//	Matcher m = p.matcher("17-MAR-11 15.52.25.000000000");
+		//	m.find();
+		//
+		//	month= m.group(2);
+
+		// Vector <Tuple <Integer, String>> ve1 = new Vector<>();
+		//BigInteger tot =  BigInteger.valueOf((long)0);
+		//BigInteger one =  new BigInteger("1");
+		//BigInteger zero =  new BigInteger("0");
+
+		Map <String, TreTuple <Integer, Integer, Integer>> mp = new HashMap<>();
+		for (int i = 0; i < blah.size(); i++) {
+			Matcher m = p.matcher(blah.get(i));
+			m.find();
+
+			String name = m.group(1);
+			int speed = Integer.valueOf(m.group(2));
+			int flytime = Integer.valueOf(m.group(3));
+			int rest = Integer.valueOf(m.group(4));
+
+			mp.put(name, new TreTuple(speed, flytime, rest));
+
+		}
+		int secS = 2503;
+		int score[] = new int[mp.size()];
+		for (int sec = 1; sec <= secS; sec++) {
+			int pos = 0;
+			int roundWinner = 0;
+			int max = 0;
+			int dists[] = new int[mp.size()];
+			for (var entry : mp.entrySet()) {
+				//System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+				var tu1 = entry.getValue();
+				int speed = tu1.first;
+				int flytime = tu1.second;
+				int rest = tu1.third;
+
+				//out.print("speed: "); out.println(speed);
+				//out.print("flytime: "); out.println(flytime);
+				//out.print("rest: "); out.println(rest);
+				int dist1 = speed * flytime * (sec / (flytime+rest));
+				int left = sec % (flytime+rest);
+				//out.println("dist1: "); out.println(dist1);
+				//out.println("left: "); out.println(left);
+
+				if (left <= flytime) {
+					dist1 += (left * speed);
+				} else {
+					dist1 += (flytime * speed);
+				}
+				//out.println(dist1);
+				//if (dist1 > max) {max = dist1; roundWinner = pos; }
+				dists[pos] = dist1;
+				pos++;
+			}
+			int ma = Arrays.stream(dists).max().orElseThrow();
+			for (int ii = 0; ii < mp.size(); ii++) {
+				if (dists[ii] == ma) {
+					score[ii]++;
+				}
+			}
+		}
+		/*
+		out.println("score: ");
+		for (int ii = 0; ii < mp.size(); ii++) {
+			out.println(score[ii]);
+		}
+		*/
+//		System.setOut(originalOut);
+		out.print("**j_ans: ");
+		out.print(Arrays.stream(score).max().orElseThrow());
+		out.println("");
+	}
+}
+
+class Tuple<X,Y > {
+	public final X first;
+	public final Y second;
+
+	public Tuple(X first, Y second) {
+		this.first = first;
+		this.second = second;
+	}
+	@Override
+	public boolean equals(Object o) {
+		Tuple tu2 = (Tuple) o;
+		if (this == o) return true;
+		if (!(o instanceof Tuple)) return false;
+		if (!this.first.equals(tu2.first)) {return false;}
+                if (!this.second.equals(tu2.second)) {return false;}
+
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second);
+	}
+
+}
+
+@SuppressWarnings("unchecked")
+class TreTuple<X,Y, Z> {
+        public X first;
+        public Y second;
+        public Z third;
+
+        public TreTuple(Object o) {
+                TreTuple tu2 = (TreTuple) o;
+                this.first = (X)tu2.first;
+                this.second = (Y)tu2.second;
+                this.third = (Z)tu2.third;
+        }
+        public TreTuple(X first, Y second, Z third) {
+                this.first = first;
+                this.second = second;
+                this.third = third;
+        }
+        @Override
+        public boolean equals(Object o) {
+                TreTuple tu2 = (TreTuple) o;
+                if (this == o) return true;
+                if (!(o instanceof TreTuple)) return false;
+                if (!this.first.equals(tu2.first)) {return false;}
+                if (!this.second.equals(tu2.second)) {return false;}
+                if (!this.third.equals(tu2.third)) {return false;}
+                return true;
+        }
+        @Override
+        public int hashCode() {
+                return Objects.hash(first, second, third);
+        }
+
+}
+
