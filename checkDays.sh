ee64f94742209e6436c3e46facb9ebcd9452b4b1
C.P Williams <conor.williams@gmail.com>
2025-07-21 17:29:19 +0100

    upd handi

--- checkDays.sh
+++ checkDays.sh
@@ -3,8 +3,8 @@ day=$1
 pa=$PWD
 while [ $year != 2025 ]; do
 	cd $year/day$day
-	/java year${year}_day${day}.java *i1.txt
-	/java year${year}_day${day}_2.java *i1.txt
+	/java -Xss1g -Xmx3g year${year}_day${day}.java *i1.txt
+	/java -Xss1g -Xmx3g year${year}_day${day}_2.java *i1.txt
 	year=$((year+1))
 	cd $pa
 done
