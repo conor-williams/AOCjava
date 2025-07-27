f5d4715e77469a9a59c539d2c1e2cb3c4001c84e
C.P Williams <conor.williams@gmail.com>
2025-07-27 01:35:55 +0100

    q20

---
+++ ALL.2021/year2021_day20_2.java
@@ -0,0 +1,286 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import static java.lang.System.out;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Collections;
+import java.lang.Character;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+// /java -Xmx2g year2019_day3.java *i1.txt
+
+
+
+//                        grid = Arrays.stream(gridTmp).map(char[]::clone).toArray(char[][]::new);
+// pe.sort(Comparator.comparingInt((TreTuple t) -> (int)t.third).thenComparingInt((TreTuple t) -> (int)t.second).thenComparingInt((TreTuple t) -> (int)t.first));///tuples.sort(Comparator.comparingInt(tuple -> tuple[0]));
+//Scanner scanner = new Scanner(System.in); scanner.nextLine();
+// int max = var_ints.stream().max(Integer::compare).orElseThrow();
+// int position = var_ints.indexOf(max);
+
+//                        for (var entry : mp.entrySet()) {
+// System.out.println("Key: " + entry.getKey() + ", Value: " + entry.getValue());
+//}
+//// Arrays.stream(array).forEach(row -> Arrays.fill(row, 0));
+@SuppressWarnings("unchecked")
+class year2021_day20 {
+	static int GX = 500;
+	static int PAD = 200;
+	//	        public static int maxPath = 0;
+	public static int lenx = 0;
+	public static int leny = 0;
+	public static char grid [][] = new char[GX+20][GX+20];
+	public static char grid3 [][] = new char[GX+20][GX+20];
+	public static char gridtmp [][] = new char[GX+20][GX+20];
+	static String algo = new String();
+	//    public static int already [][] = new int[leny][lenx];
+	static String line1 = new String();
+
+	public static int [][] keypad = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+
+	public static void main(String [] args) {
+		out.println("		2021 Day20.1");
+		out.flush();
+		Vector<String> blah = new Vector<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				//if (lenx == 0) {lenx = line.length();}
+				blah.add(line);
+				//leny++;
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+		//		PrintStream originalOut = System.out;
+		//		System.setOut(new PrintStream(new java.io.OutputStream() { public void write(int b) { } }));
+		/*
+		   grid = new char[leny][lenx];
+		   already = new int[leny][lenx];
+		   for (int i = 0; i < blah.size();i++) {
+		   grid[i] = blah.get(i).toCharArray();
+		   }
+		   */
+		for (int y = 0; y < GX; y++) {
+			for (int x = 0; x < GX; x++) {
+				grid[y][x] = '.';
+				gridtmp[y][x] = '.';
+			}
+			//grid[y][GX] = '\0';
+			//gridtmp[y][GX] = '\0';
+		}
+
+		leny = 0;
+		int imagePos = PAD;
+		int g3co = 0;
+		int numlines = 0;
+
+		String line1 = new String();
+		for (int i = 0; i < blah.size(); i++) {
+			line1 = blah.get(i);
+			if (blah.get(i).length() == 0) {
+				continue;
+			} else if (leny == 0) {
+				algo = new String(blah.get(i));
+			} else {
+				String line2 = new String();
+				for (int ii = 0; ii < PAD; ii++) {
+					line2 += '.';
+				}
+				line2 += blah.get(i);
+				for (int ii = line2.length(); ii < GX; ii++) {
+					line2+= '.';
+				}
+				//out.println(line2.length());
+				grid[imagePos] = line2.toCharArray();
+				imagePos++;
+			}
+			leny++;
+
+		}
+		
+
+		imagePos+= PAD;
+       		lenx = grid[PAD+10].length;
+	        leny = imagePos;
+
+		for (int r = 0; r < 50; r++) {
+			for (int y = 1; y < leny; y++) {
+				for (int x = 1; x < lenx; x++) {
+					int var_bit = 0;
+					if (y-1 >= 0 && x-1 >= 0 && grid[y-1][x-1] == '#') { var_bit |= 1 << 8;} 	
+					if (y-1 >= 0 && grid[y-1][x] == '#') { var_bit |= 1 << 7;}
+					if (y-1 >= 0 && x+1 < lenx && grid[y-1][x+1] == '#') { var_bit |= 1 << 6;}
+					if (x-1 >= 0 && grid[y][x-1] == '#') { var_bit |= 1 << 5;}
+					if (grid[y][x] == '#') { var_bit |= 1 << 4;}
+					if (x+1 < lenx && grid[y][x+1] == '#') { var_bit |= 1 << 3;}
+					if (y+1 < leny && x-1 >= 0 && grid[y+1][x-1] == '#') { var_bit |= 1 << 2;}
+					if (y+1 < leny && grid[y+1][x] == '#') { var_bit |= 1 << 1;}
+					if (y+1 < leny && x+1 < lenx && grid[y+1][x+1] == '#') { var_bit |= 1 << 0;}
+
+					gridtmp[y][x] = algo.charAt(var_bit);
+				}
+			}
+			for (int y = 0; y < leny; y++) {
+				for (int x = 0; x < lenx; x++) {
+					grid[y][x] = gridtmp[y][x];
+				}
+			}
+		}
+
+		int count = 0;
+		for (int y = 50; y < GX-50; y++) {
+			for (int x = 50; x < GX-50; x++) {
+				if (grid[y][x] == '#') {
+					count++;
+				}
+			}
+		}
+		out.print("**j_ans: ");
+		out.print(count);
+		out.println("");
+	}
+}
+
+class Tuple<X,Y > {
+	public X first;
+	public Y second;
+
+	public Tuple(X first, Y second) {
+		this.first = first;
+		this.second = second;
+	}
+	@Override
+	public boolean equals(Object o) {
+		Tuple tu2 = (Tuple) o;
+		if (this == o) return true;
+		if (!(o instanceof Tuple)) return false;
+		if (!this.first.equals(tu2.first)) {return false;}
+		if (!this.second.equals(tu2.second)) {return false;}
+
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second);
+	}
+
+}
+
+@SuppressWarnings("unchecked")
+class TreTuple<X,Y, Z> {
+	public X first;
+	public Y second;
+	public Z third;
+
+	public TreTuple(Object o) {
+		TreTuple tu2 = (TreTuple) o;
+		this.first = (X)tu2.first;
+		this.second = (Y)tu2.second;
+		this.third = (Z)tu2.third;
+	}
+	public TreTuple(X first, Y second, Z third) {
+		this.first = first;
+		this.second = second;
+		this.third = third;
+	}
+	@Override
+	public boolean equals(Object o) {
+		TreTuple tu2 = (TreTuple) o;
+		if (this == o) return true;
+		if (!(o instanceof TreTuple)) return false;
+		if (!this.first.equals(tu2.first)) {return false;}
+		if (!this.second.equals(tu2.second)) {return false;}
+		if (!this.third.equals(tu2.third)) {return false;}
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second, third);
+	}
+
+}
+
+@SuppressWarnings("unchecked")
+class QuadTuple<X,Y, Z, W> {
+	public X first;
+	public Y second;
+	public Z third;
+	public W fourth;
+
+	public QuadTuple(X first, Y second, Z third, W fourth) {
+		this.first = first;
+		this.second = second;
+		this.third = third;
+		this.fourth = fourth;
+	}
+	@Override
+	public boolean equals(Object o) {
+		QuadTuple tu2 = (QuadTuple) o;
+		if (this == o) return true;
+		if (!(o instanceof QuadTuple)) return false;
+
+		if (!first.equals(tu2.first)) {return false;}
+		if (!second.equals(tu2.second)) {return false;}
+		if (!third.equals(tu2.third)) {return false;}
+		if (!fourth.equals(tu2.fourth)) {return false;}
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second, third, fourth);
+	}
+
+}
+
+@SuppressWarnings("unchecked")
+class CinqTuple<X,Y, Z, V, W> {
+	public X first;
+	public Y second;
+	public Z third;
+	public V fourth;
+	public W fifth;
+
+	public CinqTuple(X first, Y second, Z third, V fourth, W fifth) {
+		this.first = first;
+		this.second = second;
+		this.third = third;
+		this.fourth = fourth;
+		this.fifth = fifth;
+	}
+	@Override
+	public boolean equals(Object o) {
+		CinqTuple tu2 = (CinqTuple) o;
+		if (this == o) return true;
+		if (!(o instanceof CinqTuple)) return false;
+
+		if (!first.equals(tu2.first)) {return false;}
+		if (!second.equals(tu2.second)) {return false;}
+		if (!third.equals(tu2.third)) {return false;}
+		if (!fourth.equals(tu2.fourth)) {return false;}
+		if (!fifth.equals(tu2.fifth)) {return false;}
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second, third, fourth, fifth);
+	}
+
+}
+
