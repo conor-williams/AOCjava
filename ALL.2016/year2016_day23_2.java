04e83dfc372ec597188b452d6d19429bb9342007
C.P Williams <conor.williams@gmail.com>
2025-08-03 14:28:33 +0100

    q23

---
+++ ALL.2016/year2016_day23_2.java
@@ -0,0 +1,87 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.util.Arrays;
+import java.math.BigInteger;
+import java.util.Scanner;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Vector;
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+import static java.lang.System.out;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.Iterator;
+import java.util.Objects;
+import java.util.Collections;
+import java.lang.Character;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.*;
+import java.io.*;
+import java.lang.*;
+
+@SuppressWarnings("unchecked")
+class year2016_day23_2 {
+
+	public static int [][] keypad = {{1, 2, 3}, {4, 5, 6}, {7, 8, 9}};
+	public static void main(String [] args) {
+		out.println("		2016 Day23.2");
+		out.flush();
+		Vector<String> blah = new Vector<>();
+		try (BufferedReader br = new BufferedReader(new FileReader(args[0]))) {
+			String line;
+			while ((line = br.readLine()) != null) {
+				blah.add(line);
+			}
+		} catch (IOException e) {
+			e.printStackTrace();
+		}	
+		PrintStream originalOut = System.out;
+		System.setOut(new PrintStream(new java.io.OutputStream() { public void write(int b) { } }));
+
+		int ans = 1;
+		for (int ii = 1; ii <= 12; ii++) {
+			ans *= ii;
+		}
+		out.println(ans);
+		Pattern p3 = Pattern.compile("(cpy|jnz) (-?\\d+) ([a-z])");
+		Matcher m3 = p3.matcher(blah.get(19));
+		m3.find();
+		Matcher m4 = p3.matcher(blah.get(20));
+		m4.find();
+		ans += Integer.valueOf(m3.group(2)) * Integer.valueOf(m4.group(2));
+
+		System.setOut(originalOut);
+		out.print("**j_ans: ");
+		out.print(ans);
+		out.println("");
+	}
+}
+
+class Tuple<X,Y > {
+	public final X first;
+	public final Y second;
+
+	public Tuple(X first, Y second) {
+		this.first = first;
+		this.second = second;
+	}
+	@Override
+	public boolean equals(Object o) {
+		Tuple tu2 = (Tuple) o;
+		if (this == o) return true;
+		if (!(o instanceof Tuple)) return false;
+		if (!this.first.equals(tu2.first)) {return false;}
+		if (!this.second.equals(tu2.second)) {return false;}
+
+		return true;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(first, second);
+	}
+
+}
+
