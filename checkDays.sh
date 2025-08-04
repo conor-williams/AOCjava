246f3a21185e1344b6107e06836a6d14f407c707
C.P Williams <conor.williams@gmail.com>
2025-08-04 20:57:11 +0100

    q25

--- checkDays.sh
+++ checkDays.sh
@@ -8,7 +8,9 @@ pa=$PWD
 while [ $year != 2025 ]; do
 	cd $year/day$day
 	/java -Xss1g -Xmx3g year${year}_day${day}.java *i1.txt
-	/java -Xss1g -Xmx3g year${year}_day${day}_2.java *i1.txt
+	if [ @$day != '@25' ]; then
+		/java -Xss1g -Xmx3g year${year}_day${day}_2.java *i1.txt
+	fi
 	year=$((year+1))
 	cd $pa
 done
